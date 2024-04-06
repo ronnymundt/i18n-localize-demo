2c075c8d90729d0be23687dcb11c2bb6d8d92d27
Ronny Mundt <r.mundt@gmx.net>
2024-04-06 11:00:00 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'i18n-localize-demo';
+export class AppComponent implements OnInit{
+  constructor(
+    private translate: TranslateService
+  ) { }
+
+  ngOnInit() {
+    this.initI18n();
+  }
+
+  private initI18n() {
+    this.translate.setDefaultLang('de_DE'); // default fallback language
+    this.translate.use('de_DE'); // use german language
+
+  }
+
+  onTranslationButtonClick() {
+    this.translate.use(this.translate.currentLang === 'de_DE' ? 'en_EN' : 'de_DE');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import {HttpClient, provideHttpClient} from "@angular/common/http";
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })),
+  ]
 };
+
+// Factory zum laden der Sprachdateien aus asset/i18n.
+export function httpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
